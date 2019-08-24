885f52c89a1ba1e0dc4a21b86c6ae21a966dd86f
Nari Yang <yangnari93@naver.com>
2019-08-24 15:04:26 +0900

    second commit

---
+++ src/cube.js
@@ -0,0 +1,4 @@
+//default 한번만 선언이 되어야 한다
+export default function cube(x) {
+	return x* x * x;
+}
---
+++ src/curcle.js
@@ -0,0 +1,9 @@
+//export 가져다가 쓰기위해..
+
+export var area = function(r) {
+	return Math.PI * r * r;
+};
+
+export var circumference = function (r) {
+	return 2 * Math.PI * r;
+};
